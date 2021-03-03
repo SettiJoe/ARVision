9f1280428d6ecfdceb2cc77a92555a440c2a3327
SettiJoe <j.settipani@gmail.com>
2021-03-03 11:29:40 -0600

    Added Unity Project

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+    public NavMeshSurface surface;
+    public NavMeshAgent agent;
+
+    public Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            surface.BuildNavMesh();
+            SetPlayerDestination();
+        }
+    }
+
+    private void SetPlayerDestination()
+    {
+        var gotHit = Physics.Raycast(mainCamera.transform.position, Vector3.down, out var cameraBaseHit);
+        if (!gotHit)
+            return;
+
+        agent.ResetPath();
+        agent.Warp(cameraBaseHit.point);
+
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+}
